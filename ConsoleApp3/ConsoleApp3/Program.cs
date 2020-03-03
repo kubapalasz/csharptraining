367e09431ebb604b6f41d23a19c88c2dc0af0ccd
Jakub Pałaszyński <japa@edc.dk>
2020-03-03 11:35:21 +0100

    First simple calculator

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,83 +1,27 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            DateTime asa = new DateTime();
-
-            Human kuba = new Human("18");
-            Human aga = new Human("12");
-
-
-
-            var justa = new Human
-            {
-                Age = 33,
-                Height = "178",
-                SkinColor = "White"
-            };
-
-            var justa2 = new Human();
-            justa.Age = 33;
-
-            Console.WriteLine("Hello " + kuba.Height + "!");
-            Console.WriteLine($"Hello {kuba.Height}!");
-            Console.WriteLine($"Hello {aga.Height}!");
             Console.Clear();
 
-            Console.WriteLine($"Hello {justa.AgeInDays()}!");
+            Console.WriteLine("Podaj pierwsza cyfre");
+            string firstNumberString = Console.ReadLine();
+            int firstNumber = int.Parse(firstNumberString);
 
-            var enteredLine = Console.ReadLine();
+            Console.WriteLine("Podaj drugą cyfre");
+            string secondNumberString = Console.ReadLine();
+            int secondNumber = int.Parse(secondNumberString);
 
-            Console.WriteLine($"You have entered '{enteredLine}'!");
 
+            int result = firstNumber + secondNumber;
 
-            var typedKey = Console.ReadKey();
+            Console.WriteLine($"Wynik = {result}"); // Interpolacja $ i zmienne w {}
 
-            Console.WriteLine($"You have typed '{typedKey.KeyChar}'!");
-
-            ConsoleKeyInfo currentKey = Console.ReadKey();
-
-            while (currentKey.Key != ConsoleKey.Escape)
-            {
-                Console.WriteLine($"> '{currentKey.KeyChar}'!");
-                currentKey = Console.ReadKey();
-            }
-
-            // if | for | foreach | switch 
-            /*
-             *
-             *
-             */
-
+            Console.ReadKey();
         }
-    }
-
-    public class Human
-    {
-        public Human()
-        {
-        }
-
-        public Human(string height)
-        {
-            Height = height;
-            SkinColor = "Red";
-        }
-
-        public string Height { get; set; }
-        public string SkinColor { get; set; }
-        public int Age { get; set; }
-
-        public int AgeInDays()
-        {
-            return Age * 365;
-        }
-
     }
 }
