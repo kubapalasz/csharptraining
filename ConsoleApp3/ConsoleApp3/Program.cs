54162f520fc5a89064f33b94fd8ba28a2077a330
Jakub Pałaszyński <japa@edc.dk>
2020-03-03 12:02:22 +0100

    Walidacja

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -7,15 +7,37 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.Clear();
-
             Console.WriteLine("Podaj pierwsza cyfre");
             string firstNumberString = Console.ReadLine();
-            int firstNumber = int.Parse(firstNumberString);
+            int firstNumber;
+            bool conversionResult = int.TryParse(firstNumberString, out firstNumber);
+
+            while (conversionResult == false)
+            {
+                Console.WriteLine("Nie podałeś liczby! Spróbuj jeszcze raz!");
+                Console.ReadKey();
+
+                Console.Clear();
+                Console.WriteLine("Podaj pierwsza cyfre");
+                firstNumberString = Console.ReadLine();
+                conversionResult = int.TryParse(firstNumberString, out firstNumber);
+            }
 
             Console.WriteLine("Podaj drugą cyfre");
             string secondNumberString = Console.ReadLine();
-            int secondNumber = int.Parse(secondNumberString);
+            int secondNumber;
+            conversionResult = int.TryParse(secondNumberString, out secondNumber);
 
+            while (conversionResult == false)
+            {
+                Console.WriteLine("Nie podałeś liczby! Spróbuj jeszcze raz!");
+                Console.ReadKey();
+
+                Console.Clear();
+                Console.WriteLine("Podaj drugą cyfre");
+                secondNumberString = Console.ReadLine();
+                conversionResult = int.TryParse(secondNumberString, out secondNumber);
+            }
 
             int result = firstNumber + secondNumber;
 
