4090faba085c1d5bfd0b6083083c36fa82c32528
Jakub Palaszynski <kubapalasz@gmail.com>
2020-03-08 21:33:35 +0100

    #3 Added method body and fixed unit tests

--- ConsoleApp3/Math.Services.Tests/SumatorTests.cs
+++ ConsoleApp3/Math.Services.Tests/SumatorTests.cs
@@ -7,13 +7,13 @@ namespace Math.Services.Tests
         [Theory]
         [InlineData(null, 0)]
         [InlineData("", 0)]
-        [InlineData("-1", 0)]
+        [InlineData("-1", -1)]
         [InlineData("0", 0)]
         [InlineData("1", 1)]
         [InlineData("1,2,3", 6)]
         [InlineData("1 , 2 , 3 ", 6)]
         [InlineData("1 , -2 , 3 ", 2)]
-        [InlineData("1 ,  , 3 ", 0)]
+        [InlineData("1 ,  , 3 ", 4)]
         public void FromCsv_VariousInput_Valid(string input, int expected)
         {
             // Act
--- ConsoleApp3/Math.Services/Sumator.cs
+++ ConsoleApp3/Math.Services/Sumator.cs
@@ -1,11 +1,31 @@
-﻿namespace Math.Services
+﻿using System.Linq;
+
+namespace Math.Services
 {
     public class Sumator
     {
         public static int FromCsv(string csv)
         {
+            var sumResult = 0;
 
-            return 0;
+            if (string.IsNullOrEmpty(csv))
+            {
+                return sumResult;
+            }
+
+            var numberCandidates = csv.Split(',').ToList();
+
+            foreach (var numberCandidate in numberCandidates)
+            {
+                int number;
+
+                if (int.TryParse(numberCandidate, out number))
+                {
+                    sumResult += number;
+                }
+            }
+
+            return sumResult;
         }
     }
 }
