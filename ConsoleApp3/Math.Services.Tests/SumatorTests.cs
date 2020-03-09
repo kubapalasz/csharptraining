69c0a71e45818a1d332d61252fbe0fa72c5c4402
Jakub Palaszynski <kubapalasz@gmail.com>
2020-03-09 20:58:15 +0100

    3# Added new test case - to omit non numbers

--- ConsoleApp3/Math.Services.Tests/SumatorTests.cs
+++ ConsoleApp3/Math.Services.Tests/SumatorTests.cs
@@ -5,16 +5,17 @@ namespace Math.Services.Tests
     public class SumatorTests
     {
         [Theory]
-        [InlineData(null, 0)]
-        [InlineData("", 0)]
-        [InlineData("-1", -1)]
-        [InlineData("0", 0)]
-        [InlineData("1", 1)]
-        [InlineData("1,2,3", 6)]
-        [InlineData("1 , 2 , 3 ", 6)]
-        [InlineData("1 , -2 , 3 ", 2)]
-        [InlineData("1 ,  , 3 ", 4)]
-        [InlineData("1,2,3,4", 10)] 
+        [InlineData(null,   0)]
+        [InlineData("",     0)]
+        [InlineData("-1",   -1)]
+        [InlineData("0",    0)]
+        [InlineData("1",    1)]
+        [InlineData("1,2,3",     6)]
+        [InlineData("1 , 2 , 3 ",    6)]
+        [InlineData("1 , -2 , 3 ",   2)]
+        [InlineData("1 ,  , 3 ",    4)]
+        [InlineData("1,2,3,4",  10)]
+        [InlineData("1,2,kuba,3,4", 10)]
         public void FromCsv_VariousInput_Valid(string input, int expected)
         {
             // Act
