ab9a97985b4da3e9f1584194c6cba8a5ee828799
Rafał Miłosz <rafaello.rm83@gmail.com>
2020-03-14 14:30:43 +0100

    Find the Largest Number in an Array #4 (added tests)

--- ConsoleApp3/Math.Services.Tests/LargestNumberTests.cs
+++ ConsoleApp3/Math.Services.Tests/LargestNumberTests.cs
@@ -10,7 +10,12 @@ namespace Math.Services.Tests
         [InlineData(new[] {"-5","6","-4"}, "6")]        
         [InlineData(new[] {"-54","-7","-8"}, "-7")]        
         [InlineData(new[] {"12","15", " 8 "}, "15")]        
-        [InlineData(new[] {"12"}, "12")]        
+        [InlineData(new[] {"12"}, "12")]
+        [InlineData(new[] {"12","", "46"},"46")]
+        [InlineData(new[] {"12","  ","25"},"25")]
+        [InlineData(new[] {"12",null}, "12")]
+        [InlineData(new[] {""}, "")]
+        [InlineData(new[] {"test"},"")]
         public void MaxFromAray_VariousInput_Valid(string[] input, string expected)
         {
             // Act
