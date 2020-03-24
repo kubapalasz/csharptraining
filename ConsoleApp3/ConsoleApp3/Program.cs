1f27783b6284776ae8d01ccd3d14f912ccb8176c
rafaello-rm <rafaello.rm83@gmail.com>
2020-03-24 20:11:49 +0100

    #8 add new service + tests + new case in Program.cs

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -50,7 +50,35 @@ namespace ConsoleApp3
                         Console.ReadKey();
                         break;
                     }
-                    
+
+
+                case "08-R":
+                    // Dodaj tutaj swój kod ;-)
+                    Console.WriteLine("Jestem prostą aplikacją eliminującą liczby nieparzyste :-)");
+                    if (args.Length == 1)   // jeśli użytkownik nie przekaże parametrów w trakcie wywołania aplikacji to zostanie o nie poproszony
+                    {
+                        Console.WriteLine("Podaj liczbę lub wciśnij 'W':");
+                        ConsoleKeyInfo currentKey = Console.ReadKey();
+                        List<string> userArguments = new List<string>();
+                        while (currentKey.Key != ConsoleKey.W)
+                        {
+                            userArguments.Add(Convert.ToString(currentKey.KeyChar) + Console.ReadLine());
+                            Console.WriteLine("Podaj kolejną liczbę lub wciśnij 'W':");
+                            currentKey = Console.ReadKey();
+                        }
+                        var wynik = ElimanteOddNumbers.RemoveOddNumbers(userArguments.ToArray());
+                        Console.WriteLine();
+                        Console.WriteLine($"Podane liczby bez nieprzystych = {wynik}");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        var wynik = ElimanteOddNumbers.RemoveOddNumbers(args);
+                        Console.WriteLine($"Podane liczby bez nieprzystych = {wynik}"); // Interpolacja $ i zmienne w {}
+                        Console.ReadKey();
+                        break;
+                    }
             }
 
             Console.ReadKey();
