e923fbfb7eb153254df69ee376b3ea3619699b4a
rafaello-rm <rafaello.rm83@gmail.com>
2020-03-24 20:11:49 +0100

    #8 add new service + tests + new case in Program.cs

---
+++ ConsoleApp3/Math.Services.Tests/ElimaneOddNumbersTests.cs
@@ -0,0 +1,35 @@
+using Xunit;
+
+namespace Math.Services.Tests
+{
+    public class ElimaneOddNumbersTests
+    {
+        [Theory]
+        [InlineData(new[] {"1", "5", "7"}, "")]
+        [InlineData(new[] {"54", "1", "0"}, "54, 0")]
+        [InlineData(new[] {"-5", "6", "-4"}, "6, -4")]
+        [InlineData(new[] {"-54", "-7", "-8"}, "-54, -8")]
+        [InlineData(new[] {"12", "15", " 8 "}, "12, 8")]
+        [InlineData(new[] {"12"}, "12")]
+        [InlineData(new[] {"12", "", "46"}, "12, 46")]
+        [InlineData(new[] {"12", "  ", "25"}, "12")]
+        [InlineData(new[] {"12", null}, "12")]
+        [InlineData(new[] {""}, "")]
+        [InlineData(new[] {"test"}, "")]
+        [InlineData(null, "")]
+        [InlineData(new[] {"ciag-znakow-nie-bedacy-liczba"}, "")]
+        [InlineData(new[] {"1.0"}, "")]
+        [InlineData(new[] {"1.2"}, "")]
+        [InlineData(new[] {"1.333333"}, "")]
+        [InlineData(new[] { "1,0" }, "")]
+        [InlineData(new[] { "1 0" }, "")]
+        public void MaxFromAray_VariousInput_Valid(string[] input, string expected)
+        {
+            // Act
+            var actual = ElimanteOddNumbers.RemoveOddNumbers(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ConsoleApp3/Math.Services/ElimanteOddNumbers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace Math.Services
+{
+    public class ElimanteOddNumbers
+    {
+        public static List<int> RemoveOddNumbers(int[] array)
+        {           
+            
+            var listNumbers = array.ToList();
+            foreach (int currentValue in listNumbers.ToList())
+            {                
+                if (currentValue % 2 != 0)
+                {
+                    listNumbers.Remove(currentValue);
+                }                
+            }            
+            return listNumbers;
+        }
+
+        public static string RemoveOddNumbers(string[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return string.Empty;
+            }
+                        
+            int[] numbersArray = Array.ConvertAll(array, Converter);
+
+            var validIntegers = new List<int>();
+            foreach (var intigerCandidate in array)
+            {
+                if (int.TryParse(intigerCandidate, out var validInteger))
+                {
+                    validIntegers.Add(validInteger);
+                }
+            }
+            var withOutOddNumbers = RemoveOddNumbers(validIntegers.ToArray());
+            try
+            {
+                var strNumbers = withOutOddNumbers.Select(i => i.ToString(CultureInfo.InvariantCulture)).Aggregate((s1, s2) => s1 + ", " + s2);
+                return strNumbers;
+            }
+            catch (Exception exception)
+            {
+
+            }
+            finally
+            {
+
+            }
+            return string.Empty;            
+        }
+
+
+        // int.Parse() <<<<<<
+        private static int Converter(string input)
+        {
+            try
+            {
+                return int.Parse(input);
+            }
+            catch (Exception exception)
+            {
+                // log error
+            }
+            finally
+            {
+                // close db/http connection 
+            }
+
+            return int.MinValue;
+        }
+    }
+}
