15d03be4e4fad0196aa6d203a045cd52677ebe38
Jakub Pałaszyński <japa@edc.dk>
2020-03-24 22:17:35 +0100

    #8 a bit of code unification

--- ConsoleApp3/Math.Services/ElimanteOddNumbers.cs
+++ ConsoleApp3/Math.Services/ElimanteOddNumbers.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
+using Math.Services.Extensions;
 
 namespace Math.Services
 {
     public class ElimanteOddNumbers
     {
-        public static List<int> RemoveOddNumbers(int[] array)
+        // "string[] array" > List<string>
+        public static string RemoveOddNumbers(List<string> listOfString)
+        {
+            var result = string.Empty;
+
+            if (listOfString == null || listOfString.Count == 0)
+            {
+                return result;
+            }
+
+            // Zrobic z tego Tool > Extension method
+            var withOutOddNumbers = RemoveOddNumbers(listOfString.ToIntList());
+
+            if (!withOutOddNumbers.Any())
+            {
+                return result;
+            }
+
+            var strNumbers = withOutOddNumbers
+                    .Select(i => i.ToString(CultureInfo.InvariantCulture))
+                    .Aggregate((s1, s2) => s1 + ", " + s2);
+            return strNumbers;
+        }
+
+        private static List<int> RemoveOddNumbers(List<int> listNumbers)
         {           
-            
-            var listNumbers = array.ToList();
             foreach (int currentValue in listNumbers.ToList())
             {                
                 if (currentValue % 2 != 0)
@@ -19,60 +41,6 @@ namespace Math.Services
                 }                
             }            
             return listNumbers;
-        }
-
-        public static string RemoveOddNumbers(string[] array)
-        {
-            if (array == null || array.Length == 0)
-            {
-                return string.Empty;
-            }
-                        
-            int[] numbersArray = Array.ConvertAll(array, Converter);
-
-            var validIntegers = new List<int>();
-            foreach (var intigerCandidate in array)
-            {
-                if (int.TryParse(intigerCandidate, out var validInteger))
-                {
-                    validIntegers.Add(validInteger);
-                }
-            }
-            var withOutOddNumbers = RemoveOddNumbers(validIntegers.ToArray());
-            try
-            {
-                var strNumbers = withOutOddNumbers.Select(i => i.ToString(CultureInfo.InvariantCulture)).Aggregate((s1, s2) => s1 + ", " + s2);
-                return strNumbers;
-            }
-            catch (Exception exception)
-            {
-
-            }
-            finally
-            {
-
-            }
-            return string.Empty;            
-        }
-
-
-        // int.Parse() <<<<<<
-        private static int Converter(string input)
-        {
-            try
-            {
-                return int.Parse(input);
-            }
-            catch (Exception exception)
-            {
-                // log error
-            }
-            finally
-            {
-                // close db/http connection 
-            }
-
-            return int.MinValue;
         }
     }
 }
